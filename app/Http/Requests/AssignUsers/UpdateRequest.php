b0019c065ed42ed715319af4ad6684e69d217198
Teguhriyadi <ilham.teguh55@gmail.com>
2026-07-20 13:39:39 +0700

    Create & Update Validation

---
+++ app/Http/Requests/AssignUsers/UpdateRequest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Requests\AssignUsers;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateRequest extends FormRequest
+{
+    public function rules(): array
+    {
+        return [
+            'event_id' => 'required',
+            'user_id' => 'required',
+            'jabatan' => 'required'
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'event_id.required' => 'Nama Event Wajib Diisi',
+            'user_id.required' => 'Nama User Wajib Diisi',
+            'jabatan.required' => 'Jabatan Wajib Diisi'
+        ];
+    }
+}
