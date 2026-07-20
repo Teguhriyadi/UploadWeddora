8641ccb3a23b47033c1f18c2559d0d4add88aba4
Teguhriyadi <ilham.teguh55@gmail.com>
2026-07-20 13:05:21 +0700

    Add Request Cabang

---
+++ app/Http/Requests/Cabang/CreateRequest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Requests\Cabang;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreateRequest extends FormRequest
+{
+    public function rules(): array
+    {
+        return [
+            'nama' => 'required',
+            'kota' => 'required',
+            'alamat' => 'required'
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'nama.required' => 'Nama Cabang Wajib Diisi',
+            'kota.required' => 'Kota Cabang Wajib Diisi',
+            'alamat.required' => 'Alamat Cabang Wajib Diisi'
+        ];
+    }
+}
