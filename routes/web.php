76eaea2052cc05a795cc59db5b38fa39743374ec
Teguhriyadi <ilham.teguh55@gmail.com>
2026-07-21 13:27:53 +0700

    Update Routing

--- routes/web.php
+++ routes/web.php
@@ -23,13 +23,6 @@ Route::get("/", function() {
     return redirect()->to("/login");
 });
 
-Route::prefix('welcome-display')->group(function () {
-    Route::get('/', [WelcomeDisplayController::class, 'index'])
-        ->name('welcome.display');
-    Route::get('/latest', [WelcomeDisplayController::class, 'latest'])
-        ->name('welcome.latest');
-});
-
 Route::get('/qr/{kode_token}', [ScanQRGuestController::class, 'poster']);
 
 Route::middleware(["web", "guest"])->group(function () {
@@ -40,6 +33,12 @@ Route::middleware(["web", "guest"])->group(function () {
 });
 
 Route::middleware(["web", "autentikasi"])->group(function () {
+    Route::prefix('welcome-display')->group(function () {
+        Route::get('/', [WelcomeDisplayController::class, 'index'])
+            ->name('welcome.display');
+        Route::get('/latest', [WelcomeDisplayController::class, 'latest'])
+            ->name('welcome.latest');
+    });
     Route::prefix("modules")->group(function () {
         Route::get("/dashboard", [AppController::class, "dashboard"]);
 
